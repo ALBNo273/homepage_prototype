f45ecdfac62976bc68bd1772f517fb481c9227b7
Yuki Umeda <albireo0725.mikumiku@gmail.com>
2016-08-30 16:01:57 +0000

    Minor fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
-# ページごとの完全なタイトルを返す
-def full_title(page_title = '')
-  base_title = "Cygnus"
-  if page_title.empty?
-    base_title
-  else
-    page_title + " | " + base_title
+module ApplicationHelper
+
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "Cygnus"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
   end
+  
 end
